5e08d7d51f792f33705e6dd189e6bc4d615eb508
dr.dimitru <ceo@veliov.com>
2021-04-21 21:09:03 +0300

    👨‍🔬 Auto-tests update

--- meteor-root-test.js
+++ meteor-root-test.js
@@ -1,10 +1,13 @@
-Tinytest.add('Meteor.rootPath (see console for path)', function(test){
-  console.log('Meteor.rootPath is: ' + Meteor.rootPath);
+import { Meteor } from 'meteor/meteor';
+import './meteor-root.js';
+
+Tinytest.add('Meteor.rootPath (see console for path)', (test) => {
+  console.log(`Meteor.rootPath is: ${Meteor.rootPath}`);
   test.isTrue(/\.meteor/.test(Meteor.rootPath));
   test.equal(Meteor.rootPath, process.cwd());
 });
 
-Tinytest.add('Meteor.absolutePath (see console for path)', function(test){
-  console.log('Meteor.absolutePath is: ' + Meteor.absolutePath);
+Tinytest.add('Meteor.absolutePath (see console for path)', (test) => {
+  console.log(`Meteor.absolutePath is: ${Meteor.absolutePath}`);
   test.equal(Meteor.absolutePath, process.cwd().split('/.meteor/')[0]);
 });
